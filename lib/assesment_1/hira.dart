a98a6922c30843cd76480674f1f69ca689d25729
Hira Tariq <i.hiratariq@gmail.com>
2023-07-18 17:40:54 +0500

    FI-as1-Hira

--- lib/assesment_1/hira.dart
+++ lib/assesment_1/hira.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 
-class FIAhmad extends StatelessWidget {
-  const FIAhmad({super.key});
+class FIHira extends StatelessWidget {
+  const FIHira({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Ahmad"),
+        title: const Text("Hira"),
       ),
       body: const Center(
-        child: Text("Hello! I am Ahmad"),
+        child: Text("Hi! I am Hira"),
       ),
     );
   }
