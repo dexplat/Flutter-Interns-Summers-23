0f8460dfc1cd681eff2c9f0cd95c83a35e5484c1
MASH679 <aaleali666@gmail.com>
2023-07-18 20:48:11 +0500

    Mutahar's file

---
+++ lib/assesment_1/fi_Mutahar.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class FIAhmad extends StatelessWidget {
+  const FIAhmad({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("MUTAHAR"),
+      ),
+      body: const Center(
+        child: Text("Hello! I MUTAHAR"),
+      ),
+    );
+  }
+}
