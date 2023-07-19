afb9e5b641271dc2f74cd01c9a0efa4b0f7b9ec7
Muhammad Hamza <hamza.6.shakeel@gmail.com>
2023-07-19 15:30:42 +0500

    a2 completed

---
+++ lib/assesments/assesment_2/fi_a2_hamza_ui/fi_a2_hamza_home.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class FIA2HamzaHomeScreen extends StatelessWidget {
+  const FIA2HamzaHomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text('hahhahaha'),
+    );
+  }
+}
