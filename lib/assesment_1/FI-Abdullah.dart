20a561e5542795fe7f742f0e90876f5464198780
Abdullah-Abbasi903 <abbasiabdullah672@gmail.com>
2023-07-19 12:40:21 +0500

    fi-asses-1

---
+++ lib/assesment_1/FI-Abdullah.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class FIAbdullah extends StatelessWidget {
+  const FIAbdullah({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Made by Abdullah"),
+      ),
+      body: const Center(
+        child: Text("Hello to all I am Abdullah Abbasi"),
+      ),
+    );
+  }
+}
