a2dddde3d03446ac38b8ce63eeb47b301511ab0d
uzair1103 <uzairimran408@gmail.com>
2023-07-19 12:29:36 +0500

    FI_assg_1-First Page Updated

---
+++ lib/assesment_1/fi_uzair.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class FIUzair extends StatelessWidget {
+  const FIUzair({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Uzair Imran"),
+      ),
+      body: const Center(
+        child: Text("Hello, I am Uzair"),
+      ),
+    );
+  }
+}
